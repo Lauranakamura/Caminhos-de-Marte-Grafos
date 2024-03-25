fee70a75b1ef20a44110a5b84d6fddf57660ce68
Laura Nakamura <112010242+Lauranakamura@users.noreply.github.com>
2024-03-25 09:35:37 -0300

    Hash Duplo e Hash Linear feito

---
+++ apCaminhosEmMarte/HashDuplo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HashDuplo<Tipo> : ITabelaDeHash<Tipo>
+  where Tipo : IRegistro<Tipo>, IComparable<Tipo>
+{
+    private readonly int tamanho;
+    private readonly Tipo[] tabela;
+
+    public HashDuplo(int tamanho)
+    {
+        this.tamanho = tamanho;
+        this.tabela = new Tipo[tamanho];
+    }
+
+    public List<string> Conteudo() // O for está errado
+    {
+        List<string> conteudo = new List<string>();
+
+        for (int i = 0; i == tamanho; i++)
+        {
+            if (tabela[i] != null)
+            {
+                conteudo.Add(tabela[i].ToString());
+            }
+        }
+
+        return conteudo;
+    }
+
+    public bool Existe(Tipo item, out int onde)
+    {
+        int hash = Hash(item.Chave);
+        int incremento = Incremento(item.Chave);
+        onde = hash;
+
+        while (tabela[onde] != null)
+        {
+            if (tabela[onde].Equals(item))
+            {
+                return true;
+            }
+
+            onde = (onde + incremento) % tamanho;
+        }
+
+        return false;
+    }
+
+    public int Hash(string chave)
+    {
+        int hash = 0;
+        foreach (char c in chave)
+        {
+            hash = (hash + c) % tamanho;
+        }
+        return hash;
+    }
+
+    public void Inserir(Tipo item)
+    {
+        int onde;
+        if (!Existe(item, out onde))
+        {
+            tabela[onde] = item;
+        }
+    }
+
+    public bool Remover(Tipo item)
+    {
+        int onde;
+        if (Existe(item, out onde))
+        {
+            tabela[onde] = default(Tipo);
+            return true;
+        }
+        return false;
+    }
+
+    List<string> ITabelaDeHash<Tipo>.Conteudo()
+    {
+        throw new NotImplementedException();
+    }
+
+    private int Incremento(string chave)
+    {
+        int hashSecundario = 7 - (Hash(chave) % 7);
+        return hashSecundario;
+    }
+}
+
---
+++ apCaminhosEmMarte/HashLinear.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HashLinear<Tipo> : ITabelaDeHash<Tipo>
+      where Tipo : IRegistro<Tipo>, IComparable<Tipo>
+{
+
+    private const int TamanhoTabela = 100; // Tamanho da tabela hash
+
+    private Tipo[] tabela; // Armazena os itens da tabela hash
+    private bool[] indicesOcupados; // Indica se um índice da tabela está ocupado ou não
+
+
+    public HashLinear()
+    {
+        tabela = new Tipo[TamanhoTabela];
+        indicesOcupados = new bool[TamanhoTabela];
+    }
+
+
+    public List<string> Conteudo()
+    {
+         throw new NotImplementedException();
+    }
+
+  public int Hash(string chave)
+  {
+        int somaValores = 0;
+
+        foreach (char c in chave)
+        {
+            somaValores += (int)c;
+        }
+
+        return somaValores % TamanhoTabela;
+    }
+
+  bool ITabelaDeHash<Tipo>.Existe(Tipo item, out int onde)
+  {
+        onde = Hash(item.Chave);
+
+        while (indicesOcupados[onde])
+        {
+            if (item.Chave == tabela[onde].Chave)
+            {
+                return true;
+            }
+
+            onde = (onde + 1) % TamanhoTabela; // Avança para o próximo índice usando uma estratégia de Hash Linear
+        }
+
+        return false;
+    }
+
+  void ITabelaDeHash<Tipo>.Inserir(Tipo item)
+  {
+        int onde;
+
+        if (!((ITabelaDeHash<Tipo>)this).Existe(item, out onde))
+        {
+            tabela[onde] = item;
+            indicesOcupados[onde] = true;
+        }
+    }
+
+  bool ITabelaDeHash<Tipo>.Remover(Tipo item)
+  {
+        int onde;
+
+        if (((ITabelaDeHash<Tipo>)this).Existe(item, out onde))
+        {
+            tabela[onde] = default(Tipo);
+            indicesOcupados[onde] = false;
+            return true;
+        }
+
+        return false;
+    }
+}
+
